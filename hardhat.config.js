37d2c1238f9886e6b58d979e6863c3b74de55b28
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2022-11-09 02:28:30 +0530

    Contract Created

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,12 +7,12 @@ require( "@nomiclabs/hardhat-ethers");
 module.exports = {
   solidity: "0.8.7",
   networks:{
-    mumbai: {
-      url:'https://black-icy-haze.matic-testnet.discover.quiknode.pro/mktp-a945e075e34e7ef1e96e424d6cc01a21aa79d006/',
+  goerli  : {
+      url:'https://aged-intensive-violet.ethereum-goerli.discover.quiknode.pro/dee808ac3a1c8dbbdb8fbc03d7ab989585f523c6/',
       accounts:['0794e7ce27e4c3b54bc1de24e3b1c8774095bb75cdbe44f879ff42b7660a6f3a'],
     }
   },
   etherscan: {
-    apiKey:'IFV6388YUDJQQAT24KE62B5QTMAWBXUKDG'
+    apiKey:'RUS49Q72R5CSRQ79BXV4Y438WZHDFEZVTJ'
   }
 };
